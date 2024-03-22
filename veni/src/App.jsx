5da753f0da3993a1b02c18c66271819bfa968e36
DiegoFNav <diegonavarro12703@gmail.com>
2024-03-22 15:58:58 -0700

    Added proper filtering to dogs, project complete.

--- veni/src/App.jsx
+++ veni/src/App.jsx
@@ -1,30 +1,56 @@
-import './App.css'
-import { useState } from 'react';
+import './App.css';
+import { useState} from 'react';
+
 const api_key = import.meta.env.VITE_APP_ACCESS_KEY;
 
 function App() {
   const [dogImage, setDogImage] = useState(null);
   const [dogBreeds, setDogBreeds] = useState([]);
   const [banList, setBanList] = useState([]);
+  const [currentDogIndex, setCurrentDogIndex] = useState(0);
+  const [banned, setBanned] = useState(false);
 
   const displayedKeys = ['name', 'life_span', 'breed_group', 'origin', 'weight'];
 
-  const url = `https://api.thedogapi.com/v1/images/search?limit=1&has_breeds=1`;
+  const url = `https://api.thedogapi.com/v1/images/search?limit=20&has_breeds=1`;
+
 
   const handleDiscoverClick = async () => {
     try {
+      if (currentDogIndex < dogBreeds.length - 1 && !banned) {
+        setCurrentDogIndex(prevIndex => prevIndex + 1);
+        setDogImage(dogBreeds[currentDogIndex + 1].url);
+        return;
+      }
+      setBanned(false);
+
       const response = await fetch(url, {
         headers: {
-          'x-api-key': api_key
-        }
+          'x-api-key': api_key,
+        },
       });
+
       if (!response.ok) {
         throw new Error('Failed to fetch dog image');
       }
+
       const data = await response.json();
-      setDogImage(data[0].url);
-      setDogBreeds(data[0].breeds);
-      console.log(data[0].breeds);
+      const filteredDogs = data.filter(dog => {
+          for (const ban of banList) {
+            const [key, value] = Object.entries(ban);
+            if (dog.breeds[0][key[0]] === key[1]) {
+              return false;
+            }
+          }
+          return true;
+        })
+        .map(dog => ({
+          url: dog.url,
+          breeds: dog.breeds,
+        }));
+      setDogImage(filteredDogs[0].url);
+      setDogBreeds(filteredDogs);
+      setCurrentDogIndex(0);
     } catch (error) {
       console.error(error);
     }
@@ -32,6 +58,7 @@ function App() {
 
   const handleBreedAttributeClick = (key, value) => {
     setBanList([...banList, { [key]: value }]);
+    setBanned(true);
   };
 
   return (
@@ -41,14 +68,14 @@ function App() {
           <div className='veni_card'>
             <h1>Veni Vici!</h1>
             <p>Discover dogs from your wildest dreams!</p>
-            {dogBreeds.map((breed, index) => (
+            {dogBreeds[currentDogIndex]?.breeds.map((breed, index) => (
               <div key={index}>
                 {Object.entries(breed)
                   .filter(([key, _]) => displayedKeys.includes(key))
                   .map(([key, value]) => (
                     <button key={key} onClick={() => handleBreedAttributeClick(key, value)}>
-                      {key === 'weight' && typeof value === 'object' // Check if key is 'weight' and value is an object
-                        ? `${value.imperial} lbs` // If yes, display 'imperial' value and append 'lbs'
+                      {key === 'weight' && typeof value === 'object'
+                        ? `${value.imperial} lbs`
                         : `${value}`}
                     </button>
                   ))}
@@ -65,9 +92,7 @@ function App() {
             <div key={index}>
               {Object.entries(breed).map(([key, value], i) => (
                 <p key={i}>
-                  {key === 'weight' && typeof value === 'object' // Check if key is 'weight' and value is an object
-                    ? `${value.imperial} lbs` // If yes, display 'imperial' value and append 'lbs'
-                    : `${value}`}
+                  {key === 'weight' && typeof value === 'object' ? `${value.imperial} lbs` : `${value}`}
                 </p>
               ))}
             </div>
@@ -75,7 +100,7 @@ function App() {
         </div>
       </div>
     </>
-  )
+  );
 }
 
 export default App;
